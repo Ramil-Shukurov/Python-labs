73d5f9a14d7c0a890c09963677619991c91faea1
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2022-10-30 15:40:19 -0700

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -8,11 +8,7 @@
 
 
 def main():
-    length = abs(float(input("length:")))
-    width = abs(float(input("width:")))
-    #length, width = map(float, input("").split())
-    area = (length) * width
-    print(f"Area: {area:.2f}")
+   
 
 if __name__ == "__main__":
     main()
